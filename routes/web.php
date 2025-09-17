ab664ffce0b7316254b35ae6e70cfce8f64bb379
Lethary <boudetk@saintjo.org>
2025-09-17 10:39:06 +0200

    Premier Contrôleur

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return view('test');
+Route::get('/page', function () {
+    return view('page');
 });
+
+Route::get('/test', [TestController::class, 'index']);
