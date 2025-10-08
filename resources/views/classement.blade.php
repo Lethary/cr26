ebf9bb1e9c2fa76416c976ca1b6f233232e1de41
Lethary <boudetk@saintjo.org>
2025-10-08 11:47:34 +0200

    Mise en place du Model Classement pour l'utilisation du model MVC

--- resources/views/classement.blade.php
+++ resources/views/classement.blade.php
@@ -1,27 +1,6 @@
-<!doctype html>
-<html lang="fr" data-theme="dark">
+@extends('layouts.app') {{-- Utilise ton layout principal --}}
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Mentions Légales</title>
-  <link rel="stylesheet" href=/css/pico.grey.css>
-</head>
+@section('title', 'Classement / Emails')
 
-<body>
-  <div class="website">
-    <header class="header" role="banner">
-      <center>
-        @include ('inc.nav')
-        <h1>Classement</h1>
-      </center>
-    </header>
-
-    <main id="main" role="main" class="main">
-    </main>
-
-    @include ('inc.footer')
-  </div>
-</body>
-
-</html>
+@section('content')
+@endsection
---
+++ resources/views/exportation.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('title', 'Exportation')
+
+@section('content')
+@endsection
